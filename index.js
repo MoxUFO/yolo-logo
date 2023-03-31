674107360946ce98956fc40cc4c9b68163abed34
MOX UFO <mox.ufo@gmail.com>
2023-03-31 15:04:57 -0400

    finished test and logo makers

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
+//imports CLI class
 const CLI = require('./lib/cli')
 
 const cli = new CLI()
-
+// entry point for the app
 cli.run()
--- tests/shapes.test.js
+++ tests/shapes.test.js
@@ -1,5 +1,6 @@
+//This imports shapes to be tested
 const shapes = require('../lib/shapes')
-
+// all test in this section test the functionality of the render method.
 describe('Shape',() => {
     test('should return a confirmation string when render() is called', () => {
         const shape = new shapes.Shape();
@@ -9,7 +10,7 @@ describe('Shape',() => {
 })
 describe('Cirlce',() => {
     test('should return a svg string when render() is called', () => {
-        const expectedString = `<svg version="1.1" width="300" height="200"> 
+        const expectedString = `<svg version="1.1" viewBox="0 0 300 200" xmlns="http://www.w3.org/2000/svg" width="300" height="200"> 
             <circle cx="150" cy="100" r="80" fill="${this.logoColor}" />
             <text x="150" y="125" font-size="60" text-anchor="middle" fill="${this.textColor}">${this.text}</text>
             </svg>`;
@@ -19,9 +20,9 @@ describe('Cirlce',() => {
 })
 describe('Square',() => {
     test('should return a svg string when render() is called', () => {
-        const expectedString = `<svg version="1.1" width="300" height="200"> 
-            <rect x="100" y="85"  width="100" height="100" fill="${this.logoColor}" />
-            <text x="150" y="145" font-size="60" text-anchor="middle" fill="${this.textColor}">${this.text}</text>
+        const expectedString = `<svg version="1.1" viewBox="0 0 300 200" xmlns="http://www.w3.org/2000/svg" width="300" height="200"> 
+            <rect x="88" y="85"  width="123" height="120" fill="${this.logoColor}" />
+            <text x="150" y="160" font-size="60" text-anchor="middle" fill="${this.textColor}">${this.text}</text>
             </svg>`;
         const shape = new shapes.Square();
         expect(shape.render()).toStrictEqual(expectedString)
@@ -29,9 +30,9 @@ describe('Square',() => {
 })
 describe('Triangle',() => {
     test('should return a svg string when render() is called', () => {
-        const expectedString = `<svg version="1.1" width="300" height="200"> 
+        const expectedString = `<svg version="1.1" viewBox="0 0 300 200" xmlns="http://www.w3.org/2000/svg" width="300" height="200"> 
             <polygon points="150, 18 244, 182 56, 182" fill="${this.logoColor}" />
-            <text x="150" y="145" font-size="60" text-anchor="middle" fill="${this.textColor}">${this.text}</text>
+            <text x="150" y="160" font-size="60" text-anchor="middle" fill="${this.textColor}">${this.text}</text>
             </svg>`;
         const shape = new shapes.Triangle();
         expect(shape.render()).toStrictEqual(expectedString)
